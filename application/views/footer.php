935d25dce931b47bdbf100daf1ffa2430b260185
Afifur Rozaq <afifurrozaq@gmail.com>
2019-07-07 15:26:05 +0700

    initial

---
+++ application/views/footer.php
@@ -0,0 +1,86 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<footer class="py-5">
+	<div class="container py-md-3">
+		<div class="row footer-grids pb-md-5 pb-3">	
+			<div class="col-md-3 col-sm-6 col-6">
+				<a href="#"> <i class="fa fa-phone"></i>Call Us</a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6">
+				<a href="#"> <i class="fa fa-envelope"></i>Send Message</a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 mt-md-0 mt-2">
+				<a href="#"> <i class="fa fa-skype"></i>Skype Call</a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 mt-md-0 mt-2">
+				<a href="#"> <i class="fa fa-comment"></i>Online Chat</a>
+			</div>
+		</div>
+		
+		<div class="subscribe-grid text-center">
+			<p class="para three mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at placerat ante. Praesent nulla nunc, pretium dapibus efficitur in, auctor eget elit. Lorem ipsum dolor sit amet </p>
+			<h5>Subscribe for our latest updates</h5>
+			<p>Get
+				<span>10%</span> off on booking</p>
+			<form action="#" method="post">
+				<input class="form-control" type="email" placeholder="Subscribe" name="Subscribe" required="">
+				<button class="btn1">
+					<i class="fa fa-paper-plane"></i>
+				</button>
+			</form>
+		</div>
+	</div>
+</footer>
+<!-- //footer -->
+
+<!-- copyright -->
+<section class="copyright py-4 text-center">
+	<div class="container">
+		<p>© 2018 In Travel. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="=_blank"> W3layouts </a></p>
+	</div>
+</section>
+<!-- //copyright -->
+
+<!-- js-scripts -->		
+
+	<!-- js -->
+	<script type="text/javascript" src="<?php echo site_url('assets/js/jquery-2.2.3.min.js');?>"></script>
+	<script type="text/javascript" src="<?php echo site_url('assets/js/bootstrap.js');?>"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+	<!-- //js -->
+	
+	<!-- start-smoth-scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript" src="<?php echo site_url('assets/js/move-top.js');?>"></script>
+	<script type="text/javascript" src="<?php echo site_url('assets/js/easing.js');?>"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+	<!-- start-smoth-scrolling -->
+	
+<!-- //js-scripts -->
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,95 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Garuda Travel</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="In Travel Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+	
+	<!-- css files -->
+	<link rel="stylesheet" href="<?php echo site_url('assets/css/bootstrap.css');?>"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="<?php echo site_url('assets/css/style.css');?>" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="<?php echo site_url('assets/css/font-awesome.css');?>"> <!-- Font-Awesome-Icons-CSS -->
+	<!-- //css files -->
+	
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+	<!-- //web-fonts -->
+	
+</head>
+
+<body>
+
+<!--Header-->
+<header>
+	<div class="container agile-banner_nav">
+		<div class="row header-top">
+			<div class="col-md-5 top-left p-0">
+				<p><i class="fa fa-phone" aria-hidden="true"></i> Call us : +18044261149 </p>
+			</div>
+			<div class="col-md-7 top-right p-0">
+				<p><i class="fa fa-map-marker" aria-hidden="true"></i> Australian Travel Agency, Ravish NSW, Australia.
+			</div>
+		</div>
+	
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			
+			<h1><a class="navbar-brand" href="index.html">Garuda Travel</a></h1>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item ">
+						<a class="nav-link" style="font-size: 12px" href="index.html">Home </a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" style="font-size: 12px" href="about.html">Hotel <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="font-size: 12px" href="services.html">Rent Car</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="font-size: 12px" href="projects.html">Leadership Camp</a>
+					</li>
+					<li class="nav-item pr-lg-0">
+						<a class="nav-link pr-lg-0" style="font-size: 12px" href="contact.html">Adventure Tour</a>
+					</li>
+					<li class="dropdown nav-item">
+						<a href="#" style="font-size: 12px" class="dropdown-toggle nav-link" data-toggle="dropdown">Article
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu agile_short_dropdown">
+							<li>
+								<a style="font-size: 12px" href="pricing.html">Serba-Serbi</a>
+							</li>
+							<li>
+								<a style="font-size: 12px" href="about.html">Malang Over View</a>
+							</li>
+						</ul>
+					</li>
+					
+				</ul>
+			</div>
+		  
+		</nav>
+	</div>
+</header>
+<!--Header-->
